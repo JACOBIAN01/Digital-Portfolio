2141d8c7caa07d17396997aadf06d7628bb80c07
JACOBIAN01 <subhadeepghorai23@gmail.com>
2024-11-24 18:30:50 +0530

    Update

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
-function App() {
+const Navbar = () => {
   return (
-    <nav>
-      <ul>
-        <li>About Me</li>
-        <li>Experience</li>
-        <li>Skills</li>
-      </ul>
+    <nav className="bg-blue-900 text-white py-5">
+      <div className="container flex mx-auto justify-between">
+      <h2 className="text-2xl font-semibold">Subhadeep's Portfolio</h2>
+        <ul className="flex space-x-5 ">
+          <li className="">
+            <a href="aboutme">About Me</a>
+          </li>
+          <li>
+            <a href="exp">Experience</a>
+          </li>
+          <li>
+            <a href="skill">Skills</a>
+          </li>
+          <li>
+            <a href="contact">Contact</a>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
-}
+};
 
-
-export default App;
-
+export default Navbar;
