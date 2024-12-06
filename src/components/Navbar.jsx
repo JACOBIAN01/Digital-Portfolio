dbd70e61d79dfeefe41f44151f5059cf5b38cc9d
JACOBIAN01 <subhadeepghorai23@gmail.com>
2024-12-06 19:30:44 +0530

    Update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,13 +2,20 @@ import React from "react";
 
 
 function Navbar(){
-  return(
+  return (
     <>
       <nav className="bg-gray-800 text-white">
-          
+        <div className="flex">
+          <div>Subhadeep's Portfolio</div>
+          <div className="justify-center">
+            <a href="#">About</a>
+            <a href="#">Projects</a>
+            <a href="#">Experience</a>
+          </div>
+        </div>
       </nav>
     </>
-  )
+  );
 }
 
 
