c026050bb6c4ed8f8089ea4c564f9f94dd0bfc10
JACOBIAN01 <subhadeepghorai23@gmail.com>
2024-12-07 17:15:56 +0530

    Update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,35 @@
 import React from "react";
 
-
-function Navbar(){
+function Navbar() {
   return (
     <>
-      <nav className="bg-gray-800 text-white">
-        <div className="flex">
-          <div>Subhadeep's Portfolio</div>
-          <div className="justify-center">
-            <a href="#">About</a>
-            <a href="#">Projects</a>
-            <a href="#">Experience</a>
+      <nav>
+        <div className="bg-blue-900 text-white flex justify-between items-center p-4">
+          <div className="mx-5 text-lg font-bold">Subhadeep's Portfolio</div>
+          <div className="mx-10 space-x-10 font-semibold">
+            <a
+              href="#about"
+              className="hover:text-blue-300 transition duration-200"
+            >
+              About
+            </a>
+            <a
+              href="#experience"
+              className="hover:text-blue-300 transition duration-300"
+            >
+              Experience
+            </a>
+            <a
+              href="#skills"
+              className="hover:text-blue-300 transition duration-300"
+            >
+              Skills
+            </a>
           </div>
         </div>
       </nav>
     </>
   );
 }
-
 
 export default Navbar;
