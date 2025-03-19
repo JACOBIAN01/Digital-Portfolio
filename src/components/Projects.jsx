094cf77acc98397695d53542730066edecb01660
JACOBIAN01 <subhadeepghorai23@gmail.com>
2025-03-19 23:47:52 +0530

    Update Image

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,4 +1,6 @@
 import MedCareImage from "../assets/MedCareImage.webp"
+import MovieImage from "../assets/MovieImage.png"
+
 
 function ProjectPage(){
     return (
@@ -8,9 +10,9 @@ function ProjectPage(){
         </div>
         <div className="container flex gap-5">
           <div className="bg-blue-900 text-white rounded-sm">
-            <img className="w-auto h-48 object-cover" src={MedCareImage} />
+            <img className="w-auto h-48 object-cover" src={MovieImage} />
             <div className="text-center font-semibold gap-5">
-              <div>Title</div>
+              <div>BioScope</div>
               <p>Description</p>
             </div>
           </div>
@@ -18,7 +20,7 @@ function ProjectPage(){
           <div className="bg-blue-900 text-white rounded-sm">
             <img className="w-auto h-48 object-cover" src={MedCareImage} />
             <div className="text-center font-semibold gap-5">
-              <div>Title</div>
+              <div>MedCare</div>
               <p>Description</p>
             </div>
           </div>
