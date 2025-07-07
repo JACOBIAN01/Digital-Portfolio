8fe4899f36cf0723a8705202345e06547eb72d10
JACOBIAN01 <subhadeepghorai23@gmail.com>
2025-07-07 12:40:54 +0530

    Update

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Home from './pages/Home';
-import ProjectPage from './components/Projects';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,6 @@
 import Hero from "../components/Hero";
 import Navbar from "../components/Navbar";
 import ProjectPage from "../components/Projects";
-import UnderDev from "../components/UnderDev";
 import '../index.css'
 
 function Home() {
